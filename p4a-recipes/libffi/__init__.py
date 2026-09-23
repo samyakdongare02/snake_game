c4bbb4ebca1b607a51593d417b6a69848b3d2e50
samyakdongare02 <samyakdongare02@users.noreply.github.com>
2026-09-23 16:23:04 +0530

    Fix p4a libffi recipe: add module-level recipe instance

--- p4a-recipes/libffi/__init__.py
+++ p4a-recipes/libffi/__init__.py
@@ -1,8 +1,11 @@
 """Local libffi recipe: official release tarball ships ./configure.
 
-Skips ./autogen.sh so Ubuntu 24.04 CI does not die on
-LT_SYS_SYMBOL_USCORE / autoconf failures.
+Avoids ./autogen.sh (LT_SYS_SYMBOL_USCORE / autoconf failures on Ubuntu 24.04).
+p4a requires a module-level `recipe` instance.
 """
+
+from os.path import exists, join
+from multiprocessing import cpu_count
 
 from pythonforandroid.logger import shprint
 from pythonforandroid.recipe import Recipe
@@ -12,30 +15,37 @@ import sh
 
 
 class LibffiRecipe(Recipe):
+    name = "libffi"
     version = "3.4.4"
     url = (
         "https://github.com/libffi/libffi/releases/download/"
         "v{version}/libffi-{version}.tar.gz"
     )
-    built_libraries = {"libffi.so*": ".libs"}
+    built_libraries = {"libffi.so": ".libs"}
 
-    def build_arch(self, arch, **kwargs):
-        env = self.get_build_env(arch)
+    def build_arch(self, arch):
+        env = self.get_recipe_env(arch)
         build_dir = self.get_build_dir(arch.arch)
-
         with current_directory(build_dir):
+            # Release tarball includes a pre-generated configure script.
+            # Never run autogen.sh / autoreconf here.
+            if not exists("configure"):
+                raise Exception(
+                    "libffi release tarball missing ./configure; "
+                    "cannot build without autoconf on this runner"
+                )
             shprint(
                 sh.Command("./configure"),
-                "--host={}".format(arch.command_prefix),
-                "--prefix={}".format(build_dir),
-                "--disable-docs",
-                "--disable-multi-os-directory",
+                "--host=" + arch.command_prefix,
+                "--prefix=" + build_dir,
+                "--disable-builddir",
+                "--enable-shared",
                 _env=env,
             )
-            try:
-                import multiprocessing
+            shprint(sh.make, "-j", str(cpu_count()), "libffi.la", _env=env)
 
-                jobs = str(max(1, multiprocessing.cpu_count()))
-            except Exception:
-                jobs = "2"
-            shprint(sh.make, "-j" + jobs, _env=env)
+    def get_include_dirs(self, arch):
+        return [join(self.get_build_dir(arch.arch), "include")]
+
+
+recipe = LibffiRecipe()
